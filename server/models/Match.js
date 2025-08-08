d8c2ff8d2c2ddc824d9317de5553503c0c401434
SpaceShuttle3902 <114274159+SpaceShuttle3902@users.noreply.github.com>
2025-08-07 21:38:02 -0500

    Match.js

--- server/models/Match.js
+++ server/models/Match.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const MatchSchema = new mongoose.Schema({
-  user1: { type: String, required: true }, //you can switch string to ObjectID once done testing
-  user2: { type: String, required: true },
+  user1: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, //you can switch string to ObjectID once done testing
+  user2: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   matchedAt: { type: Date, default: Date.now }
 });
 
