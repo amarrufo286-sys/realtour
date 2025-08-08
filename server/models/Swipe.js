d45e4b6d64d25284ba366f51f9eca94a1777e7e9
SpaceShuttle3902 <114274159+SpaceShuttle3902@users.noreply.github.com>
2025-08-07 21:35:08 -0500

    Swipe.js

--- server/models/Swipe.js
+++ server/models/Swipe.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const SwipeSchema = new mongoose.Schema({
-    swiperId: { type: String, required: true },
-    swipedId: { type: String, required: true },
+    swiperId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    swipedId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     direction: {type: String, enum: ['left', 'right'], required: true},
     timestamp: {type: Date, default: Date.now }
 
